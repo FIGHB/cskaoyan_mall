3e63ae3be6efd3c0ed24c9f8d1012397ba2f762f
chenwu14 <18814384563@163.com>
2019-10-04 21:19:05 +0800

    陈武个人中心登录完成

---
+++ mall/src/main/java/com/cskaoyan/mall/vo/ChenWuWx/UserInfo.java
@@ -0,0 +1,23 @@
+package com.cskaoyan.mall.vo.ChenWuWx;
+
+public class UserInfo {
+    String nickname;
+    String avatarUrl;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+}
